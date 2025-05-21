af12f9e7c6a850978ef44d1ef72edef24143eecc
enyoukai <an.joseph0001@gmail.com>
2025-05-21 15:33:07 -0700

    properly adding spatial memory

--- architecture/models/transformer_models/early_fusion_tsfm_models.py
+++ architecture/models/transformer_models/early_fusion_tsfm_models.py
@@ -20,6 +20,7 @@ from architecture.models.transformer_models.text_cond_visual_encoder import (
     TextCondMultiCameraVisualEncoderWDoubleDet,
     NonTxMultiCameraVisualEncoder,
 )
+from architecture.models.transformer_models.imap_embedding import ImapEmbedding
 from training.offline.train_utils import load_pl_ckpt
 from utils.constants.stretch_initialization_utils import ALL_STRETCH_ACTIONS
 from utils.nn_utils import create_causal_mask, sample_action_index_from_logits
@@ -166,14 +167,17 @@ class EarlyFusionCnnTransformer(nn.Module):
         )
 
         logits = None
-        # figure out how to instantiate this
-        implicit_memory = None
+
+        batch_size = embedded_features.shape[0]
+        
+        # dont think we need memory_pos because nn.Transformer already has positional encoding
+        implicit_memory, implicit_memory_pos, implicit_memory_mask = self.imap_embedding(batch_size)
 
         for t in range(embedded_features.shape[1]):
             logits, last_hidden_state = self.decode_and_get_logits(
                 embedded_features=embedded_features[:, t : t + 1, :],
                 implicit_memory=implicit_memory, 
-                padding_mask=padding_mask,
+                padding_mask=torch.cat([implicit_memory_mask, padding_mask[:, t : t + 1]], dim=1),
             )
 
             implicit_memory = last_hidden_state[:, : -1, :]
@@ -463,56 +467,4 @@ class EarlyFusionCnnTransformerAgent(AbstractAgent):
         self.curr_t += 1
 
         return action, torch.softmax(curr_logits, -1)
-
-
-class ImapEmbedding(nn.Module):
-    def __init__(self, model_config) -> None: # model_config contains imap_size, hidden_size
-        super().__init__()
-        self.imap_size = model_config.imap_size
-        # imap consists of imap_size x imap_size embeddings of size hidden_size
-        
-        # lookup table
-        self.imap_token_embedding = nn.Embedding(self.imap_size**2, model_config.hidden_size)
-
-        # tensor of 2D coordinates for imap_size x imap_size square centered around (0,0) (flattened into a single vector of coordinates so we can pass
-        # it into the fully connected network)
-        self.imap_pos_fts = self._create_imap_pos_features(self.imap_size) 
-
-        # FFN from equation 1
-        self.imap_pos_layer = nn.Sequential(
-            nn.Linear(2, model_config.hidden_size), # (location: x, y)
-            nn.LayerNorm(model_config.hidden_size)
-        )
-            
-        self.ft_fusion_layer = nn.Sequential(
-            nn.LayerNorm(model_config.hidden_size),
-            nn.Dropout(model_config.dropout_rate),
-        )
-
-    def _create_imap_pos_features(self, imap_size):
-        x, y = torch.meshgrid(torch.arange(imap_size), torch.arange(imap_size))
-        xy = torch.stack([x, y], dim=2)
-        xy = (xy + 0.5 - imap_size / 2).float() # relative distance to the center
-        xy = xy.view(-1, 2)
-        return xy
-
-    def forward(self, batch_size):
-        '''Get the initialized imap embedding'''
-        device = self.imap_token_embedding.weight.device
-
-        # initialize embedding for each element in the imap using the lookup table
-        token_types = torch.arange(self.imap_size**2, dtype=torch.long, device=device)
-        embeds = self.imap_token_embedding(token_types)
-
-        # create embedding to capture spatial relationship between position within the imap using equation 1
-        pos_embeds = self.imap_pos_layer(self.imap_pos_fts.to(device))
-
-        embeds = embeds + pos_embeds
-        embeds = self.ft_fusion_layer(embeds)
-
-        # (imap_size**2, hidden_size) -> (batch_size, imap_size**2, hidden_size)
-        embeds = einops.repeat(embeds, 'n d -> b n d', b=batch_size) 
-        pos_embeds = einops.repeat(pos_embeds, 'n d -> b n d', b=batch_size)
-
-        return embeds, pos_embeds 
 
---
+++ architecture/models/transformer_models/imap_embedding.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import einops
+
+# https://github.com/cshizhe/onav_rim/blob/main/offline_bc/models/onav_imap_models.py
+class ImapEmbedding(nn.Module):
+    def __init__(self, model_config) -> None: # model_config contains imap_size, hidden_size
+        super().__init__()
+        self.imap_size = model_config.imap_size
+        # imap consists of imap_size x imap_size embeddings of size hidden_size
+        
+        # lookup table
+        self.imap_token_embedding = nn.Embedding(self.imap_size**2, model_config.hidden_size)
+
+        # tensor of 2D coordinates for imap_size x imap_size square centered around (0,0) (flattened into a single vector of coordinates so we can pass
+        # it into the fully connected network)
+        self.imap_pos_fts = self._create_imap_pos_features(self.imap_size) 
+
+        # FFN from equation 1
+        self.imap_pos_layer = nn.Sequential(
+            nn.Linear(2, model_config.hidden_size), # (location: x, y)
+            nn.LayerNorm(model_config.hidden_size)
+        )
+            
+        self.ft_fusion_layer = nn.Sequential(
+            nn.LayerNorm(model_config.hidden_size),
+            nn.Dropout(model_config.dropout_rate),
+        )
+
+    def _create_imap_pos_features(self, imap_size):
+        x, y = torch.meshgrid(torch.arange(imap_size), torch.arange(imap_size))
+        xy = torch.stack([x, y], dim=2)
+        xy = (xy + 0.5 - imap_size / 2).float() # relative distance to the center
+        xy = xy.view(-1, 2)
+        return xy
+
+    def forward(self, batch_size):
+        '''Get the initialized imap embedding'''
+        device = self.imap_token_embedding.weight.device
+
+        # initialize embedding for each element in the imap using the lookup table
+        token_types = torch.arange(self.imap_size**2, dtype=torch.long, device=device)
+        embeds = self.imap_token_embedding(token_types)
+
+        # create embedding to capture spatial relationship between position within the imap using equation 1
+        pos_embeds = self.imap_pos_layer(self.imap_pos_fts.to(device))
+
+        embeds = embeds + pos_embeds
+        embeds = self.ft_fusion_layer(embeds)
+
+        # (imap_size**2, hidden_size) -> (batch_size, imap_size**2, hidden_size)
+        embeds = einops.repeat(embeds, 'n d -> b n d', b=batch_size) 
+        pos_embeds = einops.repeat(pos_embeds, 'n d -> b n d', b=batch_size)
+
+        padding_mask = torch.zeros((batch_size, self.imap_size**2), dtype=torch.bool).to(device)
+
+        return embeds, pos_embeds, padding_mask
